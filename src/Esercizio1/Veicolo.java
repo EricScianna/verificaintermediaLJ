425f647df4b747d95d6be30ba58fd6beaa341105
EricScianna <scianna.eric@gmail.com>
2021-01-13 08:48:09 +0100

    start esercizio

---
+++ src/Esercizio1/Veicolo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Esercizio1;
+
+/**
+ *
+ * @author scian
+ */
+public class Veicolo {
+    
+}
